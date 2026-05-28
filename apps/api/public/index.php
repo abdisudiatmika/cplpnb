e52d1c40ad84177cb0fc8beafc2274de71db77a3
abdisudiatmika <sudiatmika.abdi@gmail.com>
2026-05-28 10:04:06 +0800

    fix: resolve public/index.php path inclusion dynamically for local and cPanel

--- apps/api/public/index.php
+++ apps/api/public/index.php
@@ -31,20 +31,13 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+} else {
+    require __DIR__.'/../../backend_cpl/vendor/autoload.php';
+    $app = require_once __DIR__.'/../../backend_cpl/bootstrap/app.php';
+}
 
 $kernel = $app->make(Kernel::class);
 
